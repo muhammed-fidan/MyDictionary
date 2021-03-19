fb56a726171ff67dfd508a24e33d338639b8ce8f
Muhammed Fidan <Muhammed Fidan@LAPTOP-VIMI1TDU>
2021-03-19 13:49:45 +0400

    4.Ders 5.Odev 03/19/21

---
+++ My_Dictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace My_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, string> idUser = new Dictionary<int, string>();
+            idUser.Add(1, "Muhammed Fidan");
+            idUser.Add(2, "Sibel Cakir");
+            idUser.Add(3, "Engin Demirog");
+
+            List();
+
+            Count();
+
+            idUser.Remove(2); //silme islemi yapar.
+
+            List();
+
+            Count();
+
+
+            void List()
+            {
+                foreach (var users in idUser)
+                {
+                    Console.WriteLine(Environment.NewLine + users);
+                }
+            }
+            Console.ReadLine();
+
+            void Count()
+            {
+                Console.Write("Eleman sayisi:" + idUser.Count + Environment.NewLine);
+            }
+        }
+
+
+
+
+    }
+}
